eab5e724eff47b3dc881edf08f272edc7419ee08
Saw <ehkalanephaung96@gmail.com>
2024-05-21 17:15:58 +0700

    change login form

--- src/components/UserLogin.vue
+++ src/components/UserLogin.vue
@@ -1,39 +1,47 @@
+
 <template>
-  <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-      <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
-    </div>
-    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-      <!-- login form -->
-      <form @submit.prevent="login" class="space-y-6" action="#" method="POST">
-        <div>
-          <div class="flex items-center justify-between">
-            <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
-          </div>
-          <div class="mt-2">
-            <input id="username" name="username" type="text" v-model="username" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-600 sm:text-sm sm:leading-6" required />
-          </div>
-        </div>
+  <div class="flex items-center justify-center h-screen px-6 bg-gray-200">
+    <div class="w-full max-w-xl p-6 bg-white rounded-md shadow-md">
+      <div class="flex items-center justify-center">
+        <span class="text-2xl font-semibold text-gray-700">Login Account</span>
+      </div>
 
-        <div>
-          <div class="flex items-center justify-between">
-            <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
-          </div>
-          <div class="mt-2">
-            <input id="password" name="password" type="password" autocomplete="current-password" required="" v-model="password" class="block w-full rounded-md border-0 py-1.5 p-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-600 sm:text-sm sm:leading-6" />
-          </div>
-        </div>
+      <form class="mt-10" @submit.prevent="login">
+        <label class="block">
+            <span class="block text-sm font-medium text-slate-700">Username</span>
+            <input type="text" v-model="username" class="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+              focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-blue-500
+              disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+              invalid:border-pink-500 invalid:text-pink-600
+              focus:invalid:border-pink-500 focus:invalid:ring-pink-500
+            "/>
+          </label>
 
-        <div>
-          <button type="submit" class="flex w-full justify-center rounded-md bg-emerald-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600">
-            Login
+          <label class="block mt-6">
+            <span class="block text-sm font-medium text-slate-700">Password</span>
+            <input type="password" v-model="password" class="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+              focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-blue-500
+              disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+              invalid:border-pink-500 invalid:text-pink-600
+              focus:invalid:border-pink-500 focus:invalid:ring-pink-500
+            "/>
+          </label>
+
+      
+        <div class="mt-10">
+          <button
+            type="submit"
+            class="w-full px-3 py-2 text-sm text-center text-white bg-emerald-600 rounded-md focus:outline-none hover:bg-emerald-500"
+          >
+            Sign in
           </button>
         </div>
       </form>
-      <p>{{ message }}</p>
+      <p class="text-red-500 mt-3">{{ message }}</p>
     </div>
   </div>
 </template>
+
 
 <script>
 import axios from 'axios';
