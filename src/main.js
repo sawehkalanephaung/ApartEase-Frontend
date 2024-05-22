cc1a1ca536168b00c317b1475b042cefcb22205c
Saw <ehkalanephaung96@gmail.com>
2024-05-22 16:09:13 +0700

    install quasar

--- src/main.js
+++ src/main.js
@@ -5,11 +5,24 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import store from './stores';
+import { Quasar } from 'quasar'
 
-const app = createApp(App)
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+import 'quasar/dist/quasar.css';
+import { Quasar } from 'quasar'
+import quasarUserOptions from './quasar-user-options'
+
+const app = createApp(App).use(Quasar, quasarUserOptions)
 app.use(createPinia())
 app.use(router)
 app.use(store); 
+app.use(Quasar,{
+  plugins:{}, // import Quasar plugin and add here
+})
 
 app.mount('#app')
 
