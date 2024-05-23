efecfae5e5874cf3690ca00401dc3f6a85a0a528
Saw <ehkalanephaung96@gmail.com>
2024-05-23 09:38:51 +0700

    uninstall quasar

--- src/main.js
+++ src/main.js
@@ -5,24 +5,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import store from './stores';
-import { Quasar } from 'quasar'
 
-// Import icon libraries
-import '@quasar/extras/material-icons/material-icons.css'
-
-// Import Quasar css
-import 'quasar/src/css/index.sass'
-import 'quasar/dist/quasar.css';
-import { Quasar } from 'quasar'
-import quasarUserOptions from './quasar-user-options'
-
-const app = createApp(App).use(Quasar, quasarUserOptions)
+const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(store); 
-app.use(Quasar,{
-  plugins:{}, // import Quasar plugin and add here
-})
 
 app.mount('#app')
 
