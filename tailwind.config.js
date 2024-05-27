7aafb42bb629280bf8d88280b0c5ed78364de392
Saw <ehkalanephaung96@gmail.com>
2024-05-27 17:47:44 +0700

    add image in login

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,31 +5,29 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'phone': '480px',  // Define custom screen size for phones
+        'tablet': '640px', // Existing tablet size
+        'laptop': '1024px', // Existing laptop size
+        'desktop': '1280px', // Existing desktop size
+      },
+    },
   },
   daisyui: {
     themes: [
       {
         mytheme: {
-          
           "primary": "#0fae84",
-                   
           "text": "#34495E",
-                   
           "accent": "#f3f4f6",
-                   
           "bg": "#f5f5f4",
-                   
           "base": "#f3f4f6",
-                   
           "info": "#0000ff",
-                   
           "success": "#00ff00",
-                   
           "warning": "#fcd34d",
-                   
           "error": "#e11d48",
-                   },
+        },
       },
     ],
   },
