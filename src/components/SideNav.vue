8ffe9580aa1613fdc1ea75dd7a89647d8344ffaa
Saw <ehkalanephaung96@gmail.com>
2024-06-09 22:59:32 +0700

    restrict user role to access user management feature

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -12,14 +12,15 @@
         <div class="flex flex-col justify-between h-full px-[20px] space-y-[10px]">
           <div class="flex flex-col justify-between space-y-[10px]">
             <!-- Add router-link for Home -->
-            <router-link to="/residentlist" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-gray-200 hover:bg-gray-200 hover:text-gray-800 transition duration-400 ease-in-out">
+            <router-link to="/home" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-gray-200 hover:bg-gray-200 hover:text-gray-800 transition duration-400 ease-in-out">
               <svg aria-hidden="true" class="w-[25px] h-[25px] fill-current mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path>
               </svg>
-              Resident List
+             Home
             </router-link>
-            <!-- User Management Dropdown -->
-            <div  v-if="isAdmin"  class="relative" @click="toggleUserManagementDropdown">
+
+           <!-- User Management Dropdown -->
+           <div  v-if="role === 'admin'" to="/user-list"  class="relative" @click="toggleUserManagementDropdown">
               <div class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-gray-200 hover:bg-gray-200 hover:text-gray-800 transition duration-400 ease-in-out cursor-pointer">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-2">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
@@ -34,26 +35,21 @@
               </div>
               <transition name="fade">
                 <div v-show="showUserManagementDropdown" class="absolute left-0 w-full mt-2 bg-white border border-gray-200 rounded-md shadow-lg z-10">
-                  <router-link to="/userlist" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">User</router-link>
-                  <router-link to="/rolelist" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Role</router-link>
+                  <router-link to="/user-list" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">User</router-link>
+                  <router-link to="/role-list" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Role</router-link>
                 </div>
               </transition>
-
-
-             <!-- display username and role-->
-             <div class="grid grid-rows-2 space-y-1 mt-20">
-                <span v-if="user && user.username">Username: {{ user.username }}</span>
-                <span v-if="user && user.role">Role: {{ user.role }}</span>
-              </div>
-       
-            </div>
+          </div>
           </div>
 
-       
-           
+          <div class="flex flex-col items-center mt-auto mb-4">
+            <p class="text-sm font-medium">{{ username }}</p>
+            <p class="text-xs text-gray-600">{{ role }}</p>
+          </div>
+
           <div class="h-[50px]">
             <div class="flex items-center justify-between">
-              <router-link to="/" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-gray-200 hover:bg-gray-300 hover:text-gray-800 transition duration-400 ease-in-out">
+              <router-link to="/" @click="logout" class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-gray-200 hover:bg-gray-300 hover:text-gray-800 transition duration-400 ease-in-out">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-2">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25" />
                 </svg>
@@ -73,7 +69,7 @@
         <div class="cursor-pointer w-[30px]" @click="toggleSideBar">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="w-[25px] h-[25px]">
             <!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
-            <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+            <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32-14.3 32 32z" />
           </svg>
         </div>
       </div>
@@ -91,6 +87,7 @@
     <!-- Main -->
   </div>
 </template>
+
 <script setup>
 import { ref, computed, onMounted, onUnmounted } from "vue";
 import { useStore } from "vuex";
@@ -99,7 +96,8 @@ import { useRouter } from "vue-router";
 const store = useStore();
 const router = useRouter();
 
-const user = computed(() => store.getters.getUser);
+const username = computed(() => store.getters.getUser?.username || "Guest");
+const role = computed(() => store.getters.getRole);
 const showDropDown = ref(false);
 const showSide = ref(true);
 const showSuccessMessage = ref(false);
@@ -115,21 +113,20 @@ const handleResize = () => {
   showSide.value = window.innerWidth >= 768;
 };
 
-// toggle user
-const toggleDrop = () => {
-  showDropDown.value = !showDropDown.value;
-};
-
-const toggleUserManagementDropdown = () => {
-  showUserManagementDropdown.value = !showUserManagementDropdown.value;
-};
-
 const logout = () => {
-  // Perform any logout logic here (e.g., clear user data, make API call)
+  // Remove token and user from local storage
+  localStorage.removeItem("token");
+  localStorage.removeItem("user");
+
+  // Clear user data in Vuex store
+  store.commit('setUser', null);
+  store.commit('setJwtToken', null);
+
   // Show the success message
   showSuccessMessage.value = true;
+
   // Redirect to login page
-  router.push("/login");
+  router.push("/");
 };
 
 onMounted(() => {
@@ -140,6 +137,16 @@ onMounted(() => {
 onUnmounted(() => {
   window.removeEventListener("resize", handleResize);
 });
+
+
+// toggle user
+const toggleDrop = () => {
+  showDropDown.value = !showDropDown.value;
+};
+
+const toggleUserManagementDropdown = () => {
+  showUserManagementDropdown.value = !showUserManagementDropdown.value;
+};
 </script>
 
 <style>
