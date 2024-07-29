1029f15e6b2391257ca5f4a52fbe97368ae9bd04
96saw <sawehkalane_phaung@cmu.ac.th>
2024-07-29 10:07:07 +0700

    refactor or rename lineID to Email

--- src/views/ResidentCreateView.vue
+++ src/views/ResidentCreateView.vue
@@ -1,59 +1,59 @@
 <template>
   <div class="flex items-center justify-center h-full">
     <div class="w-full max-w-xl p-6 bg-white rounded-md shadow-md">
-      <h3 class="text-2xl font-medium text-gray-700 text-center">Create Resident</h3>
-      <Form @submit="register" :validation-schema="schema" class="rounded px-8 pt-6 pb-8 mb-4">
+      <h3 class="text-2xl font-medium text-center text-gray-700">Create Resident</h3>
+      <Form @submit="register" :validation-schema="schema" class="px-8 pt-6 pb-8 mb-4 rounded">
         <div class="mb-4">
-          <label class="block text-gray-700 text-sm font-bold mb-2" for="roomNumber">Room No</label>
+          <label class="block mb-2 text-sm font-bold text-gray-700" for="roomNumber">Room No</label>
           <Field
             v-model="roomNumber"
             type="text"
             id="roomNumber"
             name="roomNumber"
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
             placeholder="Room Number"
           />
-          <ErrorMessage name="roomNumber" class="text-red-500 text-xs italic" />
-          <p v-if="message" class="text-error text-xs italic mt-2">{{ message }}</p>
+          <ErrorMessage name="roomNumber" class="text-xs italic text-red-500" />
+          <p v-if="message" class="mt-2 text-xs italic text-error">{{ message }}</p>
         </div>
 
         <div class="mb-4">
-          <label class="block text-gray-700 text-sm font-bold mb-2" for="name">Name</label>
+          <label class="block mb-2 text-sm font-bold text-gray-700" for="name">Name</label>
           <Field
             v-model="name"
             type="text"
             id="name"
             name="name"
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
             placeholder="Name"
           />
-          <ErrorMessage name="name" class="text-red-500 text-xs italic" />
+          <ErrorMessage name="name" class="text-xs italic text-red-500" />
         </div>
 
         <div class="mb-4">
-          <label class="block text-gray-700 text-sm font-bold mb-2" for="lineId">Line ID</label>
+          <label class="block mb-2 text-sm font-bold text-gray-700" for="lineId">Email</label>
           <Field
             v-model="lineId"
             type="text"
             id="lineId"
             name="lineId"
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            placeholder="Line ID"
+            class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+            placeholder="Email"
           />
-          <ErrorMessage name="lineId" class="text-red-500 text-xs italic" />
+          <ErrorMessage name="lineId" class="text-xs italic text-red-500" />
         </div>
 
         <div class="flex items-center justify-between mt-10">
           <button
             type="submit"
-            class="bg-emerald-500 hover:bg-emerald-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            class="px-4 py-2 font-bold text-white rounded bg-emerald-500 hover:bg-emerald-700 focus:outline-none focus:shadow-outline"
           >
             Submit
           </button>
           <button
             type="button"
             @click="cancel"
-            class="bg-slate-700 hover:bg-slate-800 text-white font-bold py-2 px-4 rounded"
+            class="px-4 py-2 font-bold text-white rounded bg-slate-700 hover:bg-slate-800"
           >
             Back
           </button>
@@ -81,7 +81,7 @@ const message = ref('');
 const schema = yup.object().shape({
   roomNumber: yup.string().required('Room number is required!'),
   name: yup.string().required('Name is required!'),
-  lineId: yup.string().required('Line ID is required!'),
+  lineId: yup.string().required('Email is required!'),
 });
 
 
